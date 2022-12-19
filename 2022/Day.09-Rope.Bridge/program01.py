34b9b5e2fc564af3e9601d40f418026348ac07da
Gustave Michel III <gixxygamma@gmail.com>
2022-12-19 09:22:15 -0600

    2022 Day 09

--- 2022/Day.09-Rope.Bridge/program01.py
+++ 2022/Day.09-Rope.Bridge/program01.py
@@ -1,5 +1,39 @@
 import sys
 
+head = [0,0]
+tail = [0,0]
+tail_positions = []
 
 for line in sys.stdin:
-    pass
+    direction, distance = [int(e) if e.isnumeric() else e for e in line.strip().split()]
+
+    for move in range(distance):
+
+        # Move head
+        if direction == 'U':
+            head[1] += 1
+        if direction == 'D':
+            head[1] -= 1
+        if direction == 'L':
+            head[0] -= 1
+        if direction == 'R':
+            head[0] += 1
+
+        # Move tail
+        x_diff = head[0]-tail[0]
+        y_diff = head[1]-tail[1]
+        
+        
+        if abs(x_diff) > 1:
+            tail[0] += 1 if x_diff > 0 else -1
+            if abs(y_diff) > 0:
+                tail[1] += 1 if y_diff > 0 else -1
+        elif abs(y_diff) > 1:
+            tail[1] += 1 if y_diff > 0 else -1
+            if abs(x_diff) > 0:
+                tail[0] += 1 if x_diff > 0 else -1
+        
+        tail_positions.append(tuple(tail))
+
+
+print(len(set(tail_positions)))
--- 2022/Day.09-Rope.Bridge/program02.py
+++ 2022/Day.09-Rope.Bridge/program02.py
@@ -1,5 +1,40 @@
 import sys
 
+rope = [[0,0] for know in range(10)]
+tail_positions = []
 
 for line in sys.stdin:
-    pass
+    direction, distance = [int(e) if e.isnumeric() else e for e in line.strip().split()]
+
+    for move in range(distance):
+        for knot in range(len(rope)):
+            if knot == 0:
+                # Move head
+                if direction == 'U':
+                    rope[knot][1] += 1
+                if direction == 'D':
+                    rope[knot][1] -= 1
+                if direction == 'L':
+                    rope[knot][0] -= 1
+                if direction == 'R':
+                    rope[knot][0] += 1
+            else:
+                # Move tails
+                x_diff = rope[knot-1][0]-rope[knot][0]
+                y_diff = rope[knot-1][1]-rope[knot][1]
+                
+                
+                if abs(x_diff) > 1:
+                    rope[knot][0] += 1 if x_diff > 0 else -1
+                    if abs(y_diff) > 0:
+                        rope[knot][1] += 1 if y_diff > 0 else -1
+                elif abs(y_diff) > 1:
+                    rope[knot][1] += 1 if y_diff > 0 else -1
+                    if abs(x_diff) > 0:
+                        rope[knot][0] += 1 if x_diff > 0 else -1
+                
+                if knot == len(rope)-1:
+                    tail_positions.append(tuple(rope[knot]))
+
+
+print(len(set(tail_positions)))
