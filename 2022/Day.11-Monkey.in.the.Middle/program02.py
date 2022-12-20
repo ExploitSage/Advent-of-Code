bc79a46167b3534bfe063db969824712da811598
ExploitSage <gixxygamma@gmail.com>
2022-12-20 10:13:09 -0600

    2022 Day 11

--- 2022/Day.11-Monkey.in.the.Middle/program02.py
+++ 2022/Day.11-Monkey.in.the.Middle/program02.py
@@ -1,5 +1,45 @@
 import sys
+from functools import reduce
 
+monkeys = []
 
 for line in sys.stdin:
-    pass
+    if 'Monkey' in line:
+        monkeys.append({})
+        monkeys[-1]['inspected'] = 0
+    elif 'Starting items' in line:
+        monkeys[-1]['items'] = [int(e) for e in line.strip().replace(',','').split(':')[1].split()]
+    elif 'Operation' in line:
+        monkeys[-1]['operation'] = line.strip().split(':')[1].strip().split('=')[1].strip()
+    elif 'Test' in line:
+        monkeys[-1]['test'] = int(line.strip().split()[-1])
+    elif 'If true' in line:
+        monkeys[-1]['true'] = int(line.strip().split()[-1])
+    elif 'If false' in line:
+        monkeys[-1]['false'] = int(line.strip().split()[-1])
+
+common_multiple = reduce(lambda x,y:x*y,sorted([monkey['test'] for monkey in monkeys]))
+
+for round in range(10000):
+    for monkey in monkeys:
+        while len(monkey['items']) > 0:
+            # Inspect
+            item = monkey['items'].pop(0)
+            monkey['inspected'] += 1
+
+            # Worry
+            operation = lambda old: eval(monkey['operation'])
+            item = operation(item)
+            
+            # Relief
+            item = item % common_multiple
+
+            # Test
+            test = lambda x: x%monkey['test']==0
+
+            if test(item):
+                monkeys[monkey['true']]['items'].append(item)
+            else:
+                monkeys[monkey['false']]['items'].append(item)
+
+print(reduce(lambda x,y:x*y,sorted([monkey['inspected'] for monkey in monkeys])[-2:]))
