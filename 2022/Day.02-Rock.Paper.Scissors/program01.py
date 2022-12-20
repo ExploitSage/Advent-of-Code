e57876efcd7343db105a64df75817acd40325c30
Gustave Michel III <gixxygamma@gmail.com>
2022-12-19 23:11:32 -0600

    Better 2022 Day 02

--- 2022/Day.02-Rock.Paper.Scissors/program01.py
+++ 2022/Day.02-Rock.Paper.Scissors/program01.py
@@ -1,21 +1,10 @@
 import sys
 
-choice_score = [('X',1),('Y',2),('Z',3)]
-
-win_conditions = [('A','Y'),('B','Z'),('C','X')]
-draw_conditions = [('A','X'),('B','Y'),('C','Z')]
-lose_conditions = [('A','Z'),('B','X'),('C','Y')]
-
 score = 0
 
 for line in sys.stdin:
-    match = tuple(line.strip().split())
-    score += (list(filter(lambda x:match[1] in x, choice_score))[0][1])
-    if match in win_conditions:
-        score += 6
-    elif match in draw_conditions:
-        score += 3
-    elif match in lose_conditions:
-        score += 0
+    match = [ord(line.strip().split()[0])-65, ord(line.strip().split()[1])-88]
+    score += (match[1]+1)
+    score += (((match[1]-match[0])+1)%3)*3
 
 print(score)
--- 2022/Day.02-Rock.Paper.Scissors/program02.py
+++ 2022/Day.02-Rock.Paper.Scissors/program02.py
@@ -1,19 +1,10 @@
 import sys
 
-draw_scores = [('A',1),('B',2),('C',3)]
-lose_scores = [('A',3),('B',1),('C',2)]
-win_scores =  [('A',2),('B',3),('C',1)]
-
 score = 0
 
 for line in sys.stdin:
-    match = tuple(line.strip().split())
-
-    if match[1] == 'X':
-        score += list(filter(lambda x:match[0] in x, lose_scores))[0][1]
-    if match[1] == 'Y':
-        score += list(filter(lambda x:match[0] in x, draw_scores))[0][1]+3
-    if match[1] == 'Z':
-        score += list(filter(lambda x:match[0] in x, win_scores))[0][1]+6
+    match = [ord(line.strip().split()[0])-65, ord(line.strip().split()[1])-88]
+    score += match[1]*3
+    score += (((match[1]+match[0])+2)%3)+1
 
 print(score)
